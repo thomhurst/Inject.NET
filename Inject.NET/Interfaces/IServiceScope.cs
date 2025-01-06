d1fbfe8d7a7be07ddf5ae08b456c4cc7cb2ced2a
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-01-06 12:18:39 +0000

    GetServices

--- Inject.NET/Interfaces/IServiceScope.cs
+++ Inject.NET/Interfaces/IServiceScope.cs
@@ -5,6 +5,7 @@ namespace Inject.NET.Interfaces;
 public interface IServiceScope : IAsyncDisposable, System.IServiceProvider
 {
     object? GetService(ServiceKey serviceKey);
+    IEnumerable<object> GetServices(ServiceKey serviceKey);
     
     IServiceProvider ServiceProvider { get; }
 }

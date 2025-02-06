741756bd858edbc97911683020342ed1ac5c8eb6
Tom Longhurst <130671542+msm-tomlonghurst@users.noreply.github.com>
2025-02-06 11:29:19 +0000

    CircularDependencyTests2

---
+++ Inject.NET.SourceGenerator.Tests/CircularDependencyTests2.cs
@@ -0,0 +1,16 @@
+﻿using Inject.NET.SourceGenerator.Tests.Options;
+
+namespace Inject.NET.SourceGenerator.Tests;
+
+public class CircularDependencyTests2 : TestsBase<DependenciesSourceGenerator>
+{
+    [Test]
+    public Task Test() => RunTest(Path.Combine(Sourcy.Git.RootDirectory.FullName,
+            "Inject.NET.Tests",
+            "CircularDependencyTests2.cs"),
+        new RunTestOptions
+        {
+            ExpectedErrors = ["error IJN0001: Conflict: Inject.NET.Tests.CircularDependencyTests2.Class1 depends on Inject.NET.Tests.CircularDependencyTests2.Class2 which depends on Inject.NET.Tests.CircularDependencyTests2.Class1"],
+        },
+        async generatedFiles => { await Assert.That(generatedFiles.Length).IsEqualTo(0); });
+}
---
+++ Inject.NET.Tests/CircularDependencyTests2.cs
@@ -0,0 +1,24 @@
+﻿using Inject.NET.Attributes;
+
+namespace Inject.NET.Tests;
+
+public partial class CircularDependencyTests2
+{
+    [Test]
+    public async Task Test()
+    {
+        var provider = await CircularDependencyServiceProvider.BuildAsync();
+        
+        await using var scope = provider.CreateTypedScope();
+    }
+
+    public interface Interface1;
+    public interface Interface2;
+    public class Class1(Interface2 @class) : Interface1;
+    public class Class2(Interface1 @class) : Interface2;
+
+    [ServiceProvider]
+    [Scoped<Interface1, Class1>]
+    [Scoped<Interface2, Class2>]
+    public partial class CircularDependencyServiceProvider;
+}

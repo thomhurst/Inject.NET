40e96fe98956aaa940efecda9399f10069770363
Tom Longhurst <130671542+msm-tomlonghurst@users.noreply.github.com>
2025-02-07 17:14:51 +0000

    More tests

--- Inject.NET.Tests/EnumerableDependencyTests.cs
+++ Inject.NET.Tests/EnumerableDependencyTests.cs
@@ -1,4 +1,3 @@
-
 using Inject.NET.Attributes;
 using Inject.NET.Extensions;
 
@@ -18,7 +17,7 @@ public partial class EnumerableDependencyTests
         await Assert.That(dependent).IsNotNull();
         await Assert.That(dependent.Dependencies).HasCount().EqualTo(3);
     }
-    
+
     [Test]
     public async Task CanResolve_Type_With_ReadOnlyListDependency()
     {
@@ -31,7 +30,7 @@ public partial class EnumerableDependencyTests
         await Assert.That(dependent).IsNotNull();
         await Assert.That(dependent.Dependencies).HasCount().EqualTo(3);
     }
-    
+
     [Test]
     public async Task CanResolve_EnumerableDependency_Directly()
     {
@@ -44,6 +43,36 @@ public partial class EnumerableDependencyTests
         await Assert.That(dependent).HasCount().EqualTo(3);
     }
 
+    [Test]
+    public async Task CanResolve_EnumerableDependency_InRegisteredOrder()
+    {
+        var serviceProvider = await EnumerableDependencyServiceProvider.BuildAsync();
+
+        await using var scope = serviceProvider.CreateScope();
+
+        var dependencies = scope.GetServices<IDependency>().ToList();
+
+        await Assert.That(dependencies).HasCount().EqualTo(3);
+        await Assert.That(dependencies[0]).IsTypeOf<Dependency1>();
+        await Assert.That(dependencies[1]).IsTypeOf<Dependency2>();
+        await Assert.That(dependencies[2]).IsTypeOf<Dependency3>();
+    }
+
+    [Test]
+    public async Task CanResolve_EnumerableDependency_MixedScopes()
+    {
+        var serviceProvider = await MixedScopeServiceProvider.BuildAsync();
+
+        await using var scope = serviceProvider.CreateScope();
+
+        var dependencies = scope.GetServices<IDependency>().ToList();
+
+        await Assert.That(dependencies).HasCount().EqualTo(3);
+        await Assert.That(dependencies[0]).IsTypeOf<Dependency1>();
+        await Assert.That(dependencies[1]).IsTypeOf<Dependency2>();
+        await Assert.That(dependencies[2]).IsTypeOf<Dependency3>();
+    }
+
     [ServiceProvider]
     [Scoped<Dependent>]
     [Scoped<Dependent2>]
@@ -51,6 +80,23 @@ public partial class EnumerableDependencyTests
     [Scoped<IDependency, Dependency2>]
     [Scoped<IDependency, Dependency3>]
     public partial class EnumerableDependencyServiceProvider;
+
+    [ServiceProvider]
+    [Transient<Dependent>]
+    [Transient<Dependent2>]
+    [Transient<IDependency, Dependency1>]
+    [Singleton<IDependency, Dependency2>]
+    [Scoped<IDependency, Dependency3>]
+    public partial class MixedScopeServiceProvider;
+
+    [Scoped<IDependency, Dependency1>]
+    public class Scope1;
+
+    [Scoped<IDependency, Dependency2>]
+    public class Scope2;
+
+    [Scoped<IDependency, Dependency3>]
+    public class Scope3;
 
     public class Dependent
     {
@@ -61,7 +107,7 @@ public partial class EnumerableDependencyTests
 
         public List<IDependency> Dependencies { get; }
     }
-    
+
     public class Dependent2
     {
         public Dependent2(IReadOnlyList<IDependency> dependencies)
